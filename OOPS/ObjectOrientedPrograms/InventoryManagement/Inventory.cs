42076e90a632f6fba479feaf5e8338426bd25d6b
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-01-17 16:21:05 +0530

    [Amit] Add. added InventoryManagement

---
+++ OOPS/ObjectOrientedPrograms/InventoryManagement/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientedPrograms
+{
+   
+    public class Inventory
+    {
+        public List<Item> Rice { get; set; }
+        public List<Item> Pulse { get; set; }
+        public List<Item> Wheat { get; set; }
+
+        public class Item
+        {
+            public string name { get; set; }
+            public int weight { get; set; }
+            public int price { get; set; }
+        }
+    }
+}
---
+++ OOPS/ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace ObjectOrientedPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InventoryDataManagement dataManagement = new InventoryDataManagement();
+            dataManagement.details();
+        }
+    }
+}
