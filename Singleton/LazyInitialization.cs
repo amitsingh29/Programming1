77ddbee210269499cfccbdad1755f82308146549
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-01-20 19:02:54 +0530

    [Amit] Add. LazyInitialization completed

---
+++ Singleton/LazyInitialization.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Singleton
+{
+    class LazyInitialization
+    {
+        public static LazyInitialization obj;
+        private LazyInitialization()
+        {
+
+        }
+        public static LazyInitialization GetObj()
+        {
+                if (obj == null)
+                {
+                    obj = new LazyInitialization();
+                }
+                return obj;
+            }
+         
+        }
+    }
+
