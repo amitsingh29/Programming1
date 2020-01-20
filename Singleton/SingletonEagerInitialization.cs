71a516278b5c960a958089bc07e741eba5986adc
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-01-20 14:39:42 +0530

    [Amit] Add. added Singleton Eager Initialization

---
+++ Singleton/SingletonEagerInitialization.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Singleton
+{
+    class SingletonEagerInitialization
+    {
+        private static SingletonEagerInitialization initialization = new SingletonEagerInitialization();
+        private SingletonEagerInitialization()
+        {
+
+        }
+        public static SingletonEagerInitialization GetSingleton()
+        {
+            return initialization;
+        }
+    }
+}
