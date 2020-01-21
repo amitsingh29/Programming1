4cc34f1243fec077050afb12840eb9e4c585c67c
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-01-21 11:10:26 +0530

    [Amit] Add. Thread Safe Singleton

---
+++ Singleton/ThreadSafeSingleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Singleton
+{
+    class ThreadSafeSingleton
+    {
+        private static ThreadSafeSingleton obj;
+        private static object obj1=new object();
+
+        /// <summary>
+        /// Prevents a default instance of the <see cref="ThreadSafeSingleton"/> class from being created.
+        /// </summary>
+        private ThreadSafeSingleton()
+        {
+            Console.WriteLine("Instance created");
+        }
+        public static ThreadSafeSingleton GetObj
+        {
+            get
+            {
+               lock(obj1)
+                {
+                    if (obj == null)
+                    {
+                        obj = new ThreadSafeSingleton();
+                    }
+                    return obj;
+                }
+            }   
+        }
+    }
+}
