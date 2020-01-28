7571bd04bbfe264b63ca0af33847d1b08c58464d
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-01-28 16:43:19 +0530

    [Amit] Add. Employee class added

---
+++ Model/Employee.cs
@@ -0,0 +1,60 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Employee.cs" company="BridgeLabz">
+//     Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Amit Singh"/>
+//-----------------------------------------------------------------------
+
+namespace EmployeeManagement.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Employee class
+    /// </summary>
+    public class Employee
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the full name.
+        /// </summary>
+        /// <value>
+        /// The full name.
+        /// </value>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the age.
+        /// </summary>
+        /// <value>
+        /// The age.
+        /// </value>
+        public string Age { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email identifier.
+        /// </summary>
+        /// <value>
+        /// The email identifier.
+        /// </value>
+        public string EmailId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the work experience.
+        /// </summary>
+        /// <value>
+        /// The work experience.
+        /// </value>
+        public int WorkExperience { get; set; }
+    }
+}
---
+++ Repository/IRepo.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IRepo.cs" company="BridgeLabz">
+//     Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Amit Singh"/>
+//-----------------------------------------------------------------------
+
+namespace EmployeeManagement.Repository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// IRepo class
+    /// </summary>
+    public interface IRepo
+    {
+    
+    }
+}
