f6b0d944aae94aa4f61b1c5321d402f389e92855
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-02-07 14:18:09 +0530

    [Amit] Add. Harmonic Number

---
+++ BasicPrograms/HarmonicNumber.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+printf "Enter the number upto you want to find Harmonic series"
+read n
+hn=1;
+sum=0;
+for((i=1;i<=n;i++))
+do	
+sum=$(echo | awk -v y=$sum -v i=$i '{print(y+1/i)}')
+	printf "1/$i + "
+done
+echo "Nth Harmonic value of the series is:$sum"
+
+
