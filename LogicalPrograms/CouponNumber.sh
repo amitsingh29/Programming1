5878fdae05e0de7720633bf0818a0afb67c1d256
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-02-10 13:00:56 +0530

    [Amit] Add. Coupon Number

---
+++ LogicalPrograms/CouponNumber.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Enter the no distinct Coupon Number"
+read num
+
+declare -a couponNumbers
+for((i=0;i<$num;i++))
+do
+couponNumber[i]=$((RANDOM%10))
+echo "values ${couponNumber[$i]}"
+done
+
+for((i=0;i<$num;i++))
+do
+ for((j=$((i+1));j<$num;j++))
+ do
+ if((couponNumber[i]==couponNumber[j]))
+ then
+ couponNumbers[i]=$((RANDOM%10))
+ fi
+ done
+ done
+
+  for((j=0;j<$num;j++))
+  do 
+  echo ${couponNumber[$j]}
+  done 
