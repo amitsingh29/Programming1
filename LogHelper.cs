b82134cb474be151a53ba4ab8c1451d4d7f4d182
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-02-11 18:05:29 +0530

    [Amit] Add. StyleCop

--- LogHelper.cs
+++ LogHelper.cs
@@ -1,15 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿//-----------------------------------------------------------------------
+// <copyright file="LogHelper.cs" company="BridgeLabz">
+//     Copyright © 2020
+// </copyright>
+// <creator name="Amit Singh Bhakuni"/>
+//-----------------------------------------------------------------------
 
 namespace LoggerFile
 {
-    class LogHelper
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// LogHelper class
+    /// </summary>
+    public class LogHelper
     {
-        public static log4net.ILog GetLogger([CallerFilePath]string filename = "")
+        /// <summary>
+        /// GetLogger method
+        /// </summary>
+        /// <param name="filename">string</param>
+        /// <returns>return's filename</returns>
+        public static log4net.ILog GetLogger([CallerFilePath]string filename = " ")
         {
             return log4net.LogManager.GetLogger(filename);
         }
