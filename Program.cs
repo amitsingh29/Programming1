8a7142c08e6ff0da2cf6aec9eb8e48e18afc08a3
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-02-11 17:43:22 +0530

    [Amit] Add. Log Helper

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ [assembly: log4net.Config.XmlConfigurator(Watch=true)]
+
+namespace LoggerFile
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = LogHelper.GetLogger();
+
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hello Amit");
+            log.Error("This is my error message");
+            Console.ReadLine();
+                      
+        }
+    }
+}
