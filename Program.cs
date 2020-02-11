3212b57aa2b26803310e396580c0606c32de7617
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-02-11 17:55:03 +0530

    [Amit] Refactor. comments added

--- Program.cs
+++ Program.cs
@@ -1,23 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="BridgeLabz">
+//     Copyright © 2020
+// </copyright>
+// <creator name="Amit Singh Bhakuni"/>
+//-----------------------------------------------------------------------
 
- [assembly: log4net.Config.XmlConfigurator(Watch=true)]
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
 namespace LoggerFile
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Program class
+    /// </summary>
+    public class Program
     {
+        /// <summary>
+        /// The log
+        /// </summary>
         private static readonly log4net.ILog log = LogHelper.GetLogger();
 
-        static void Main(string[] args)
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+       public static void Main(string[] args)
         {
            Console.WriteLine("Hello Amit");
             log.Error("This is my error message");
-            Console.ReadLine();
-                      
+            Console.ReadLine();             
         }
     }
 }
