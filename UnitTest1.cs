5937173ac3301276fda7b7811a8191463550d4c3
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-02-19 15:59:49 +0530

    [Amit] Refactor. removed unneccessary files and added changes

---
+++ UnitTest1.cs
@@ -0,0 +1,90 @@
+//-----------------------------------------------------------------------
+// <copyright file="UnitTest1.cs" company="BridgeLabz">
+//     Copyright © 2020
+// </copyright>
+// <creator name="Amit Singh"/>
+//-----------------------------------------------------------------------
+
+namespace XUnitUserRegistration
+{
+    using System;
+    using Xunit;
+
+    /// <summary>
+    /// UnitTest1 class
+    /// </summary>
+    public class UnitTest1
+    {
+        /// <summary>
+        /// checks the test case
+        /// </summary>
+        [Fact]
+        public void TestFirstName()
+        {
+            Assert.True(UserRegistration.FirstName("Amit"));
+        }
+
+        /// <summary>
+        /// checks the test case
+        /// </summary>
+        [Fact]
+        public void TestFirstNameNull()
+        {
+            Assert.False(UserRegistration.FirstName(string.Empty));
+        }
+
+        /// <summary>
+        /// checks the test case
+        /// </summary>
+        [Fact]
+        public void TestLastName()
+        {
+            Assert.True(UserRegistration.LastName("Singh"));
+        }
+
+        /// <summary>
+        /// checks the test case
+        /// </summary>
+        [Fact]
+        public void TestLastNameNull()
+        {
+            Assert.False(UserRegistration.LastName(string.Empty));
+        }
+
+        /// <summary>
+        /// checks the test case
+        /// </summary>
+        [Fact]
+        public void TestEmailId()
+        {
+            Assert.True(UserRegistration.EmailId("amit.singh29@gmail.com"));
+        }
+
+        /// <summary>
+        /// checks the test case
+        /// </summary>
+        [Fact]
+        public void TestEmailIdNull()
+        {
+            Assert.False(UserRegistration.EmailId(string.Empty));
+        }
+
+        /// <summary>
+        /// checks the test case
+        /// </summary>
+        [Fact]
+        public void TestPassword()
+        {
+            Assert.True(UserRegistration.Password("Admin@123"));
+        }
+
+        /// <summary>
+        /// checks the test case
+        /// </summary>
+        [Fact]
+        public void TestPasswordNull()
+        {
+            Assert.False(UserRegistration.Password(string.Empty));
+        }
+    }
+}
---
+++ UserRegistration.cs
@@ -0,0 +1,69 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="UserRegistration.cs" company="BridgeLabz">
+//     Copyright © 2020
+// </copyright>
+// <creator name="Amit Singh"/>
+//-----------------------------------------------------------------------
+namespace XUnitUserRegistration
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// UserRegistration class
+    /// </summary>
+    public class UserRegistration
+    {
+        /// <summary>
+        /// this is FirstName method
+        /// </summary>
+        /// <param name="input">string input</param>
+        /// <returns>boolean value</returns>
+        public static bool FirstName(string input)
+        {
+            return Regex.IsMatch(input, "^[a-zA-Z]+$");
+        }
+
+        /// <summary>
+        /// this is LastName method
+        /// </summary>
+        /// <param name="input">string input</param>
+        /// <returns>boolean value</returns>
+        public static bool LastName(string input)
+        {
+            return Regex.IsMatch(input, "^[a-zA-Z]+$");
+        }
+
+        /// <summary>
+        /// this is Age method
+        /// </summary>
+        /// <param name="input">string input</param>
+        /// <returns>boolean value</returns>
+        public static bool Age(string input)
+        {
+            return Regex.IsMatch(input, "^[0-9]+$");
+        }
+
+        /// <summary>
+        /// this is EmailId method
+        /// </summary>
+        /// <param name="input">string input</param>
+        /// <returns>boolean value</returns>
+        public static bool EmailId(string input)
+        {
+            return Regex.IsMatch(input, "^([a-z0-9._]+)@(gmail|outlook|yahoo).(com|org)$");
+        }
+
+        /// <summary>
+        /// this is Password method
+        /// </summary>
+        /// <param name="input">string input</param>
+        /// <returns>boolean value</returns>
+        public static bool Password(string input)
+        {
+            return Regex.IsMatch(input, "^(^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$)");
+        }
+    }
+}
