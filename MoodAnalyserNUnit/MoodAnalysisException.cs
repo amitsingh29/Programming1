7f19b871ac00b991234271a7d9c77b890ed05a5e
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-02-25 17:11:41 +0530

    [Amit] Refactor. Stylecop added in MoodAnalysisException

--- MoodAnalyserNUnit/MoodAnalysisException.cs
+++ MoodAnalyserNUnit/MoodAnalysisException.cs
@@ -1,28 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//-----------------------------------------------------------------------
+// <copyright file="MoodAnalysisException.cs" company="BridgeLabz">
+//     Copyright © 2020
+// </copyright>
+// <creator name="Amit Singh"/>
+//-----------------------------------------------------------------------
 
 namespace MoodAnalyserNUnit
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    ///  MoodAnalysisException class
+    /// </summary>
     public class MoodAnalysisException : Exception
     {
+        /// <summary>
+        /// string instance variable
+        /// </summary>
         public string message;
+
+        /// <summary>
+        /// Exception_Type instance variable
+        /// </summary>
+        public Exception_Type Type;
+
+        /// <summary>
+        /// enum
+        /// </summary>
         public enum Exception_Type
         {
-             Empty , Null , NoSuchClass
+            /// <summary>
+            /// Emptymood Exception_Type
+            /// </summary>
+            Empty,
+
+            /// <summary>
+            /// NullMood Exception_Type
+            /// </summary>
+            Null,
+
+            /// <summary>
+            /// NoSuchClass Exception_Type
+            /// </summary>
+            NoSuchClass
         }
-        public Exception_Type type;
-        public MoodAnalysisException(Exception_Type type , string message)
+
+        /// <summary>
+        /// custom constructor
+        /// </summary>
+        /// <param name="Type">Exception_Type typ</param>
+        /// <param name="message">string messag</param>
+        public MoodAnalysisException(Exception_Type type, string message)
         {
-            this.type = type;
+            this.Type = type;
             this.message = message;
 
         }
+
+        /// <summary>
+        /// overridden Message method
+        /// </summary>
         public override string Message
         {
             get
             {
-                return message;
+                return this.message;
             }
         }
     }
