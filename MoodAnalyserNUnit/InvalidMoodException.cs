1250193cd0bf3fde33a092c470033924b7a9303e
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-02-25 17:16:06 +0530

    [Amit] Refactor. InvalidMoodException

--- MoodAnalyserNUnit/InvalidMoodException.cs
+++ MoodAnalyserNUnit/InvalidMoodException.cs
@@ -1,11 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//-----------------------------------------------------------------------
+// <copyright file="InvalidMoodException.cs" company="BridgeLabz">
+//     Copyright © 2020
+// </copyright>
+// <creator name="Amit Singh"/>
+//-----------------------------------------------------------------------
 
 namespace MoodAnalyserNUnit
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// InvalidMoodException class
+    /// </summary>
     public class InvalidMoodException : Exception
     {
+        /// <summary>
+        /// overridden Message method
+        /// </summary>
         public override string Message
         {
             get
