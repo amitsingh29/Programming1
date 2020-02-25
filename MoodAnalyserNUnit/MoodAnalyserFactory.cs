5ee2685eeed07a7fa0354cf6a28dc1f6e3c7f794
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-02-25 16:25:07 +0530

    [Amit] Refactor. MoodAnalyserFactory method

--- MoodAnalyserNUnit/MoodAnalyserFactory.cs
+++ MoodAnalyserNUnit/MoodAnalyserFactory.cs
@@ -1,16 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Text;
+﻿//-----------------------------------------------------------------------
+// <copyright file="MoodAnalyserFactory.cs" company="BridgeLabz">
+//     Copyright © 2020
+// </copyright>
+// <creator name="Amit Singh"/>
+//-----------------------------------------------------------------------
 
 namespace MoodAnalyserNUnit
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Text;
+
+    /// <summary>
+    ///  MoodAnalyserFactory class
+    /// </summary>
     public class MoodAnalyserFactory
     {
+        /// <summary>
+        /// default constructor
+        /// </summary>
         public MoodAnalyserFactory()
         {
-
         }
+
+        /// <summary>
+        /// MoodAnalyser method
+        /// </summary>
+        /// <param name="className">string parameter</param>
+        /// <returns>object type</returns>
         public static object MoodAnalyser(string className)
         {
             Type type = Type.GetType("MoodAnalyserNUnit." + className);
@@ -21,6 +39,7 @@ namespace MoodAnalyserNUnit
                    object obj = Activator.CreateInstance(type);
                     return obj;
                 }
+
                 throw new MoodAnalysisException(MoodAnalysisException.Exception_Type.NoSuchClass, "No Such Class");
             }
             catch (MoodAnalysisException exception)
